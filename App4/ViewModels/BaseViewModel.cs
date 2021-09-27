6320ef37a374f67531035c73d26431eafde6c890
David-barce <71525685+David-barce@users.noreply.github.com>
2021-09-27 14:11:14 -0500

    Agregar archivos de proyecto.

---
+++ App4/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace App4.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+    }
+}
